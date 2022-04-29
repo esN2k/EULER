2a3c7b1f5cbeafb351fe64c09611d0a8a9d905da
Doğukan 'Esen' Balaman <dodikbalaman@gmail.com>
2022-04-30 02:01:32 +0300

    Problem 2 çözümü ve Açıklamalar

--- lib/problem_1.dart
+++ lib/problem_1.dart
@@ -1,4 +1,12 @@
-// ignore_for_file: unused_local_variable, avoid_print, unused_import
+/// Problem 1: 3 ve 5'in katları
+///
+/// Örnek olarak 10'dan küçük 3 veya 5'in katları olan
+/// doğal sayıları sıralamak istersek:
+/// 3, 5, 6, 9 sayılarını elde ediyoruz.
+/// Bu sayıların toplamları da 23 yapar.
+///
+/// 1000'den küçük 3 veya 5'in katlarını bulunuz.
+
 import 'package:more/collection.dart';
 
 const int max = 1000;
---
+++ lib/problem_2.dart
@@ -0,0 +1,9 @@
+/// Problem 2: Çift Fibonacci sayıları
+/// 
+/// Fibonacci dizisindeki her yeni sayı, önceki 2 sayının toplamı ile oluşur.
+/// 1 ve 2 ile başlayan ilk 10 sayı:
+/// 
+/// 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+/// 
+/// 4 Milyon'u aşmayacak şekilde çift olan Fibonacci sayılarını bulunuz.
+
